ad13e7400dc3d2195c6c1b4f1cb87078f52bf996
zorin91 <alexsent@rogers.com>
2018-10-08 18:03:40 -0400

    Added a Tile class
    All tests successfully passed and slight modifications to naming and expected results in the TileTest class

---
+++ src/main/java/common/Tile.java
@@ -0,0 +1,81 @@
+package common;
+
+public class Tile {
+
+	private String colour;
+	private int rank;
+	private int xPosition;
+	private int yPosition;
+	
+	public Tile(String colour,int rank)
+	{
+		if(colour.equals("R") || colour.equals("r"))
+		{
+			this.colour = "Red";
+		}
+		else if(colour.equals("G") || colour.equals("g"))
+		{
+			this.colour = "Green";
+		}
+		else if(colour.equals("B") || colour.equals("b"))
+		{
+			this.colour = "Blue";
+		}
+		else if(colour.equals("O") || colour.equals("o"))
+		{
+			this.colour = "Orange";
+		}
+		else
+		{
+			this.colour = "NULL";
+		}
+		this.rank = rank;
+		this.xPosition = 0;
+		this.yPosition = 0;
+	}
+	
+	public Tile(String colour,int rank,int xPos, int yPos)
+	{
+		if(colour.equals("R") || colour.equals("r"))
+		{
+			this.colour = "Red";
+		}
+		else if(colour.equals("G") || colour.equals("g"))
+		{
+			this.colour = "Green";
+		}
+		else if(colour.equals("B") || colour.equals("b"))
+		{
+			this.colour = "Blue";
+		}
+		else if(colour.equals("O") || colour.equals("o"))
+		{
+			this.colour = "Orange";
+		}
+		else
+		{
+			this.colour = "NULL";
+		}
+		this.rank = rank;
+		this.xPosition = xPos;
+		this.yPosition = yPos;
+	}
+	
+	public int getRank()
+	{
+		return this.rank;
+	}
+	
+	public String getColour()
+	{
+		return this.colour;
+	}
+	public int getXPosition()
+	{
+		return this.xPosition;
+	}
+	public int getYPosition()
+	{
+		return this.yPosition;
+	}
+}
--- src/test/java/common/TileTest.java
+++ src/test/java/common/TileTest.java
@@ -4,33 +4,37 @@ import junit.framework.TestCase;
 
 public class TileTest extends TestCase{
 	
-	public void TestGetXPosition()
+	public void testGetXPosition()
 	{
 		Tile tile1 = new Tile("R",9,100,300);
 		Tile tile2 = new Tile("O",13,50,900);
+		Tile tile3 = new Tile("B",7);
 		
 		assertEquals(100,tile1.getXPosition());
 		assertEquals(50,tile2.getXPosition());
+		assertEquals(0,tile3.getXPosition());
 	}
 	
-	public void TestGetYPosition()
+	public void testGetYPosition()
 	{
 		Tile tile1 = new Tile("R",9,100,300);
 		Tile tile2 = new Tile("O",13,50,900);
+		Tile tile3 = new Tile("B",7);
 		
 		assertEquals(300,tile1.getYPosition());
 		assertEquals(900,tile2.getYPosition());
+		assertEquals(0,tile3.getYPosition());
 	}
 	
-	public void TestGetColour()
+	public void testGetColour()
 	{
 		Tile tile1 = new Tile("R",9,100,300);
 		Tile tile2 = new Tile("O",13,50,900);
 		
-		assertEquals("R",tile1.getColour());
-		assertEquals("O",tile2.getColour());
+		assertEquals("Red",tile1.getColour());
+		assertEquals("Orange",tile2.getColour());
 	}
-	public void TestGetRank()
+	public void testGetRank()
 	{
 		Tile tile1 = new Tile("R",9,100,300);
 		Tile tile2 = new Tile("O",13,50,900);
