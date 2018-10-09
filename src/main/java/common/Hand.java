14f0a615ac1871a00cf3df0374e421bf1c811f1e
SafelyFast <35852287+SafelyFast@users.noreply.github.com>
2018-10-09 00:09:28 -0400

    impl: Added inital Hand class

---
+++ src/main/java/common/Hand.java
@@ -0,0 +1,29 @@
+/**
+ * Hand class
+ * Represents a hand held by an entity
+ * Created by Jacob DiDiodato
+ * **/
+
+package common;
+
+import common.Tile;
+
+import java.util.ArrayList;
+
+public class Hand {
+	List<Tile> tiles;
+	
+	public Hand()
+	{
+		this.tiles = new ArrayList<Tile>();
+	}
+	
+	public Tile getTile(int i)
+	{
+		if (i >= this.tiles.size())
+		{
+			return null;
+		}
+		return this.tiles.get(i);
+	}
+}
