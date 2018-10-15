a63ee1a4d71e456e929c6792faabe4b91fa6d397
Jake Kendrick <43183524+JDKendrick@users.noreply.github.com>
2018-10-15 13:25:39 -0400

    test and implementation: added new test case testIsAI().
    - testIsAI() tests polymorphism to make sure an Entity class can hold an AI object (AI is the subclass of Entity).
    
    - The corresponding implementation was added in the last commit concerning the AITest.java file and thus no implementation is needed here.

--- src/test/java/common/AITest.java
+++ src/test/java/common/AITest.java
@@ -13,30 +13,35 @@ import junit.framework.TestCase;
 
 public class AITest extends TestCase {
 
-	//Tests for AIType_1 (i.e. Strategy 1) go here:	
+//---------------Tests concerning the AI class in general go here: -----------------------------------------	
+	
+	//Tests for polymorphism of the Entity and AI class (AI inherits Entity).
+	public void testIsAI() {
+		Entity ai = new AI(new AIType_1());
+		assertEquals(true, ai instanceof AI); 		
+	}
+	
+//---------------Tests for AIType_1 (i.e. Strategy 1) go here: ---------------------------------------------	
 	
 	//Tests to confirm if the AI has the right behaviour (AIType_1) initialized.
 	public void testIsType_1() {
 		AI ai = new AI(new AIType_1());
-		System.out.println(ai.behaviour instanceof AIType_1);
 		assertEquals(true, ai.behaviour instanceof AIType_1);
 	}
-	
-	//Tests for AIType_2 (i.e. Strategy 2) go here:
+
+//---------------Tests for AIType_2 (i.e. Strategy 2) go here: ---------------------------------------------	
 	
 	//Tests to confirm if the AI has the right behaviour (AIType_2) initialized.
 	public void testIsType_2() {
 		AI ai = new AI(new AIType_2());
-		System.out.println(ai.behaviour instanceof AIType_2);
 		assertEquals(true, ai.behaviour instanceof AIType_2);
 	}
 	
-	//Tests for AIType_3 (i.e. Strategy 3) go here:	
+//---------------Tests for AIType_3 (i.e. Strategy 3) go here: ---------------------------------------------	
 	
 	//Tests to confirm if the AI has the right behaviour (AIType_3) initialized.
 	public void testIsType_3() {
 		AI ai = new AI(new AIType_3());
-		System.out.println(ai.behaviour instanceof AIType_3);
 		assertEquals(true, ai.behaviour instanceof AIType_3);
 	}
 	
