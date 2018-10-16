fbda6dc2cbcddd004b23c9e4bba22383d0828d71
Jake Kendrick <43183524+JDKendrick@users.noreply.github.com>
2018-10-16 00:02:30 -0400

    Implement: added two methods to Entity.java
    - These two methods - maxCurrentPoints() and findRun() - solve the single test case in EntityTest.java
    	- Note: The test case in EntityTest.java is called "testCanPlayInitialMeld()"
    
    - maxCurrentPoints() calculates the maximum amount of points currently in an Entity's Hand. However, the implementation has not been fully completed yet and only counts runs. I will be adding more to it as the test cases in EntityTest.java get more complicated.
    
    - findRun() finds the first available run in an Entity's Hand and returns it as a ArrayList<Tile> object.

--- src/main/java/common/Entity.java
+++ src/main/java/common/Entity.java
@@ -5,7 +5,7 @@
  * **/
 package common;
 
-import common.Hand;
+import java.util.*;
 
 public class Entity {
 	/**	TODO
@@ -23,4 +23,71 @@ public class Entity {
 	{
 		
 	}
+	
+	public List<Tile> findRun(List<Tile> handList){
+		
+		List<Tile> run = new ArrayList<Tile>();
+		List<Tile> tempList = new ArrayList<Tile>();
+		
+		if(handList.isEmpty())
+			return null;
+		
+		tempList.addAll(handList);
+		run.add(tempList.get(0));
+		tempList.remove(0);	
+		
+		for(int i = 0; i < tempList.size(); i++) {
+				
+			if(tempList.isEmpty())
+				break;
+			
+			for(int j = 0; j < run.size(); j++) {	
+				if(run.get(0).getColour().equals(tempList.get(i).getColour())) {			
+					if(run.get(j).getRank() - 1 == tempList.get(i).getRank()) {
+						run.add(j, tempList.get(i));
+						tempList.remove(i);
+						i = 0;
+					}
+					else if(run.get(j).getRank() + 1 == tempList.get(i).getRank()) {
+						run.add(j + 1, tempList.get(i));
+						tempList.remove(i);	
+						i = 0;
+					}					
+				}				
+			}						
+		}
+		
+		if(run.size() < 3) {			
+			handList.remove(0);
+			return findRun(handList);
+		}
+		else
+			return run;
+
+	}
+	
+	public int maxCurrentPoints() {
+		
+		List<Meld> runs = new ArrayList<Meld>();
+		List<Tile> hand = new ArrayList<Tile>();
+		int answer = 0;
+		hand.addAll(this.hand.tiles);
+		
+		do {
+	        runs.add(new Meld());
+	        runs.get(runs.size() - 1).tiles = findRun(hand);
+	        if(runs.get(runs.size() - 1).tiles != null) {
+	        	hand.removeAll(runs.get(runs.size() - 1).tiles);
+	        	
+	        	for(int i = 0; i < runs.get(runs.size() - 1).tiles.size(); i++)
+	        		answer += runs.get(runs.size() - 1).tiles.get(i).getRank();
+	        }
+		}
+		while(runs.get(runs.size() - 1).tiles != null);		
+		
+		System.out.println(answer);
+		
+		return answer;
+	}
+	
 }
