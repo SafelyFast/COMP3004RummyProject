13294b27bb500ecc53a8453f1a6a5c084bc4a27b
hutch47 <dudeofthedude_321@hotmail.com>
2018-10-16 18:56:08 -0400

    init: Added Player class

---
+++ src/main/java/common/Player.java
@@ -0,0 +1,17 @@
+/*
+ * Player class
+ * 
+ * Requirements:
+ * 	- Communication with the GameManager and View Manager
+ *  - Functions for user input
+ */
+
+package common;
+
+public class Player extends Entity {
+	
+	public Player() {
+		
+	}
+	
+}
