d2ca8e90b7fdb0239559ec784a42ad2c11ac852b
hutch47 <dudeofthedude_321@hotmail.com>
2018-11-01 14:37:14 -0400

    init: added AIUtils for functions used in AI activity

---
+++ src/main/java/common/AIUtils.java
@@ -0,0 +1,20 @@
+package common;
+
+public class AIUtils {
+	// Plays 30 points
+	public static void playThirty(Hand h) {
+		
+	}
+	// Adds all possible cards to board melds
+	public static void addMeld(Hand h) {
+		
+	}
+	// Use existing board melds to make new melds
+	public static void rearrangeMelds(Hand h) {
+		
+	}
+	// Create a new meld with only the hand
+	public static void makeMeldFromHand(Hand h) {
+		
+	}
+}
