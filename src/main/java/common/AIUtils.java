1118e73b4fa4151c69f8f50bb01411835d58b9d2
hutch47 <dudeofthedude_321@hotmail.com>
2018-11-01 14:45:41 -0400

    Lil comment

--- src/main/java/common/AIUtils.java
+++ src/main/java/common/AIUtils.java
@@ -7,13 +7,13 @@ public class AIUtils {
 	}
 	// Adds all possible cards to board melds
 	public static void addMeld(Hand h) {
-		
 	}
 	// Use existing board melds to make new melds
 	public static void rearrangeMelds(Hand h) {
 		
 	}
 	// Create a new meld with only the hand
+	// [COMPLETED BY JAKE]
 	public static void makeMeldFromHand(Hand h) {
 		
 	}
