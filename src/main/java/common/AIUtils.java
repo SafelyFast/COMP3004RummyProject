8d4e0f63dda18892d055d0a67aca14ef32a8bbae
hutch47 <dudeofthedude_321@hotmail.com>
2018-11-04 15:28:56 -0500

    Revert "init: AIUtils class"
    This reverts commit 300d71b16e945f677ada4398be9f48fbe1138370.

--- src/main/java/common/AIUtils.java
+++ src/main/java/common/AIUtils.java
@@ -2,14 +2,14 @@ package common;
 
 public class AIUtils {
 	// Plays 30 points
-	public static void playThirty(TileManager tm, Hand h) {
+	public static void playThirty(Hand h) {
 		
 	}
 	// Adds all possible cards to board melds
-	public static void addMeld(TileManager tm, Hand h) {
+	public static void addMeld(Hand h) {
 	}
 	// Use existing board melds to make new melds
-	public static void rearrangeMelds(TileManager tm, Hand h) {
+	public static void rearrangeMelds(Hand h) {
 		
 	}
 	// Create a new meld with only the hand
