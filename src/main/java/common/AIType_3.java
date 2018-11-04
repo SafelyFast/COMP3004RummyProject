105131caea00dc82f6af927d0a4a4a1cbf5a6d68
hutch47 <dudeofthedude_321@hotmail.com>
2018-11-04 16:06:16 -0500

    main: AIType 1 2 and 3 comments and functionality

--- src/main/java/common/AIType_3.java
+++ src/main/java/common/AIType_3.java
@@ -11,9 +11,22 @@ package common;
 
 
 public class AIType_3 implements AIType{
-
+	
+	boolean hasPlayedThirty = false;
+	// In future, make performAction return an Integer
+	// Return a number if it needs to draw
+	// For now drawing cards will be done within AITypes
 	public void performAction(TileManager tM, Hand h)
 	{
+		if (hasPlayedThirty) {
+			
+		}
+		else if (maxCurrentPoints(h) >= 30) {
+			hasPlayedThirty = true;
+		}
+		else {
+			h.addTileToHand(tM.getNext());
+		}
 		
 	}
 	
