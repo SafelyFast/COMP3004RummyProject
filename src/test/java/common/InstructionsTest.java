1a18872cbecd1752d5b63a7dcd56313127b5519c
hutch47 <dudeofthedude_321@hotmail.com>
2018-11-12 18:31:31 -0500

    test: Added test case for multiple actions in a single instruction

--- src/test/java/common/InstructionsTest.java
+++ src/test/java/common/InstructionsTest.java
@@ -31,4 +31,28 @@ public class InstructionsTest extends TestCase {
 		inst.createNewMeld(m2);
 		assertEquals(2, inst.newMelds.size());
 	}
+	
+	public void testMultipleInstructions() {
+		Instructions inst = new Instructions();
+		inst.subject = new AI(new AIType_2());
+		Meld m1 = new Meld(new Tile("R", 11), new Tile("G", 11), new Tile("O", 11));
+		Meld m2 = new Meld(new Tile("R", 4), new Tile("R", 5), new Tile("R", 6));
+		Tile t1 = new Tile("R", 7);
+		Tile t2 = new Tile("R", 8);
+		Tile t3 = new Tile("B", 11);
+		Tile t4 = new Tile("R", 5);
+		Tile t5 = new Tile("O", 5);
+		Tile t6 = new Tile("G", 5);
+		
+		inst.addTiles(m1, t3);
+		inst.addTiles(m2, t1, t2);
+		inst.createNewMeld(new Meld(t4, t5, t6));
+		
+		assertEquals("Player: A2\n Played 6 tiles\n"
+				+ " 1 new meld(s) created\n"
+				+ " New meld made with 3 tiles: Red 5, Orange 5, Green 5\n"
+				+ " Tile Blue 11 added to meld Red 11, Green 11, Orange, 11\n"
+				+ " Tile Red 7 added to meld Red 4, Red 5, Red 6\n"
+				+ " Tile Red 8 added to meld Red 4, Red 5, Red 6, Red 7\n", inst.toString());
+	}
 }
