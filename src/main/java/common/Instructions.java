4b5a94306e285f29ce467b7eac6d1871619ab990
hutch47 <dudeofthedude_321@hotmail.com>
2018-11-12 18:49:06 -0500

    main: added functions to Instructions

--- src/main/java/common/Instructions.java
+++ src/main/java/common/Instructions.java
@@ -1,5 +1,6 @@
 package common;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Instructions {
@@ -8,6 +9,34 @@ public class Instructions {
 	List<Tile> tilesPlayed;
 	List<Meld> meldsChanged;
 	List<Meld> newMelds;
+	
+	public Instructions() {
+		subject = null;
+		numTilesPlayed = 0;
+		tilesPlayed = new ArrayList<Tile>();
+		meldsChanged = new ArrayList<Meld>();
+		newMelds = new ArrayList<Meld>();
+	}
+	
+	public Instructions(Entity sub) {
+		subject = sub;
+		numTilesPlayed = 0;
+		tilesPlayed = new ArrayList<Tile>();
+		meldsChanged = new ArrayList<Meld>();
+		newMelds = new ArrayList<Meld>();
+	}
+	
+	public void addTiles(Meld m, Tile ... tiles) {
+		for (Tile t : tiles) {
+			m.addMeldTile(t);
+			tilesPlayed.add(t);
+		}
+		meldsChanged.add(m);
+	}
+	
+	public void createNewMeld(Meld m) {
+		newMelds.add(m);
+	}
 	
 	public String toString() {
 		return "Player: " + this.subject + "\n Played " +  numTilesPlayed + " tiles\n";
