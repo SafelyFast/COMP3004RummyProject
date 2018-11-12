d8ec396fe4d281d52bc04638d8b6b1c69c99b3fc
hutch47 <dudeofthedude_321@hotmail.com>
2018-11-12 18:08:05 -0500

    test: Added test case for adding tiles to melds in InstructionsTest

--- src/test/java/common/InstructionsTest.java
+++ src/test/java/common/InstructionsTest.java
@@ -10,4 +10,16 @@ public class InstructionsTest extends TestCase {
 		inst.numTilesPlayed = 5;
 		assertEquals("Player: AI1\n Played 5 tiles\n", inst.toString());
 	}
+	
+	public void testAddTiles() {
+		Instructions inst = new Instructions();
+		Tile t1 = new Tile("R", 5);
+		Tile t2 = new Tile("R", 6);
+		Meld m1 = new Meld(new Tile("R", 1), new Tile("R", 2), new Tile("R", 3), new Tile("R", 4));
+		inst.addTiles(m1, t1, t2);
+		m1.addMeldTile(t1);
+		m1.addMeldTile(t2);
+		assertEquals(2, inst.numTilesPlayed);
+		assertEquals(t1, inst.meldsChanged.get(0).getTileAt(4));
+	}
 }
