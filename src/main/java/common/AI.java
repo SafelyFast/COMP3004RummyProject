SafelyFast/COMP3004RummyProject

commit b7ed4474475d88eb7a3fc674f80dfe0bebab5f1e

Bug fixes and simplifying code.
Note: functions such as playthirty do not work are broken using these changes until a Boardstate object is created. A temporary solution was created to fix this.

src/main/java/common/AI.java
@@ -29,7 +29,7 @@ public abstract class AI extends Entity implements AIType {
 					message += (meldList.get(i).getTileAt(j).getColour() + " " + meldList.get(i).getTileAt(j).getRank() + ", ");
 				
 				tm.addMeldToBoardMeld(meldList.get(i));
-				this.hand.tiles = removeFromHand(this.hand.tiles, meldList.get(i));
+				this.hand.tiles = removeFromMeld(this.hand.tiles, meldList.get(i));
 					
 			}
 				
@@ -146,7 +146,7 @@ public abstract class AI extends Entity implements AIType {
 
 	// Use existing board melds to make new melds
 	public void rearrangeMelds(TileManager tm) {
-		/*
+	/*	
 		List<Tile> tempHand = this.hand.tiles;	
 		List<Meld> boardMelds = tm.getBoardMelds();
 		List<Tile> boardTiles = convertMeldListToTileList(boardMelds);
@@ -157,14 +157,26 @@ public abstract class AI extends Entity implements AIType {
 		List<Meld> answer = getMaximumNumberOfMeldsFromHand();
 		List<Tile> newBoard = convertMeldListToTileList(answer);
 		
-			if(containsSublist(newBoard, boardTiles)) {
-				System.out.println("\n\nsuccess\n\n");
+		this.hand.tiles = tempHand;
+		
+		if(containsSublist(newBoard, boardTiles)) {
+			
+			for(int i = 0; i < boardMelds.size(); i++) {
+				
+				for(int j = 0; j < answer.size(); i++) {
+					
+					if(containsSublist())
+						
+				}
+				
 			}
-			else {
-				System.out.println("\n\nSPLOOSH\n\n");
-			}
-		*/
-	
+			
+		}
+		else {
+			System.out.println("\n\nSPLOOSH\n\n");
+		}
+		
+	*/
 	}
 
 	// Create a new meld with only the hand
@@ -180,7 +192,7 @@ public abstract class AI extends Entity implements AIType {
 					message += (meldList.get(i).getTileAt(j).getColour() + " " + meldList.get(i).getTileAt(j).getRank() + ", ");
 			
 				tm.addMeldToBoardMeld(meldList.get(i));
-				this.hand.tiles = removeFromHand(this.hand.tiles, meldList.get(i));
+				this.hand.tiles = removeFromMeld(this.hand.tiles, meldList.get(i));
 			}
 			System.out.println(message.substring(0, message.length() - 2));
 		}
@@ -275,11 +287,30 @@ public abstract class AI extends Entity implements AIType {
 			return false;
 		}
 	}
-
+/* TODO: for later usage
 	// Removes a Meld (i.e. a set of Tile objects) from the hand of an AI.
+	public void removeFromHand(Meld meld) {
 
-	// TODO Should not be applying stuff to temporary things!
-	public List<Tile> removeFromHand(List<Tile> hand, Meld meld) {
+		if (this.hand.tiles == null || meld.tiles == null)
+			return;
+
+		if (this.hand.tiles.isEmpty() || meld.tiles.isEmpty() || this.hand.tiles.size() < meld.tiles.size())
+			return;
+
+		while (!meld.tiles.isEmpty()) {
+
+			for (int i = 0; i < this.hand.tiles.size(); i++) {
+				if (this.hand.tiles.get(i).getColour().equals(meld.tiles.get(0).getColour())
+				 && this.hand.tiles.get(i).getRank() == meld.tiles.get(0).getRank()) {
+					this.hand.tiles.remove(i);
+					break;
+				}
+			}
+			meld.tiles.remove(0);
+		}
+	}
+*/	
+	public List<Tile> removeFromMeld(List<Tile> hand, Meld meld) {
 
 		List<Tile> tempHand = new ArrayList<Tile>();
 		Meld tempMeld = new Meld();
@@ -564,7 +595,7 @@ public abstract class AI extends Entity implements AIType {
 
 			tempAnswer.add(tempMeldList.get(i));
 			answerCounter += tempMeldList.get(i).getSize();
-			tempList = removeFromHand(tempList, tempMeldList.get(i));
+			tempList = removeFromMeld(tempList, tempMeldList.get(i));
 			tempMeldList.remove(i);
 
 			for (int j = 0; j < tempMeldList.size(); j++) {
@@ -575,7 +606,7 @@ public abstract class AI extends Entity implements AIType {
 				if (containsSublist(tempList, tempMeldList.get(j).tiles)) {
 					tempAnswer.add(tempMeldList.get(j));
 					answerCounter += tempMeldList.get(j).getSize();
-					tempList = removeFromHand(tempList, tempMeldList.get(j));
+					tempList = removeFromMeld(tempList, tempMeldList.get(j));
 					tempMeldList.remove(j);
 					j--;
 				}
@@ -616,7 +647,7 @@ public abstract class AI extends Entity implements AIType {
 
 			tempAnswer.add(tempMeldList.get(i));
 			answerCounter += tempMeldList.get(i).getMeldValue();
-			tempList = removeFromHand(tempList, tempMeldList.get(i));
+			tempList = removeFromMeld(tempList, tempMeldList.get(i));
 			tempMeldList.remove(i);
 
 			for (int j = 0; j < tempMeldList.size(); j++) {
@@ -627,7 +658,7 @@ public abstract class AI extends Entity implements AIType {
 				if (containsSublist(tempList, tempMeldList.get(j).tiles)) {
 					tempAnswer.add(tempMeldList.get(j));
 					tempMeldList.get(j).getMeldValue();
-					tempList = removeFromHand(tempList, tempMeldList.get(j));
+					tempList = removeFromMeld(tempList, tempMeldList.get(j));
 					tempMeldList.remove(j);
 					j--;
 				}
@@ -666,14 +697,14 @@ public abstract class AI extends Entity implements AIType {
 			tempList.addAll(this.hand.tiles);
 
 			tempAnswer += tempMeldList.get(i).getMeldValue();
-			tempList = removeFromHand(tempList, tempMeldList.get(i));
+			tempList = removeFromMeld(tempList, tempMeldList.get(i));
 			tempMeldList.remove(i);
 
 			for (int j = 0; j < tempMeldList.size(); j++) {
 
 				if (containsSublist(tempList, tempMeldList.get(j).tiles)) {
 					tempAnswer += tempMeldList.get(j).getMeldValue();
-					tempList = removeFromHand(tempList, tempMeldList.get(j));
+					tempList = removeFromMeld(tempList, tempMeldList.get(j));
 					tempMeldList.remove(j);
 					j--;
 				}

src/main/java/common/TileManager.java
@@ -121,7 +121,7 @@ public class TileManager {
 			}
 			if (success) {
 				m.updateMeldPosition(randomX, randomY);
-				System.out.println("Meld: " + m + " was played at x: " + randomX + " y: " + randomY);
+				System.out.println("Meld: " + m.ID + " was played at x: " + randomX + " y: " + randomY);
 				System.out.println("After " + attempts + " attempts");
 				return true;
 			}

src/test/java/common/AIUtilsTest.java
@@ -35,6 +35,7 @@ import junit.framework.TestCase;
 			assertEquals(30, ((AI) GM.players.get(1)).calculateMaxPoints()); //4a1 <-- Should "Pass" - There is one set meld of 30 points exactly. 
 			((AI) GM.players.get(1)).playThirty(TM);			
 			assertEquals(0, GM.players.get(1).hand.getSize()); //Should "Pass" - The only meld in it's hand has been played.
+			assertEquals(1, TM.getBoardMeldSize());
 			
 			
 			

commit 4e3b3df23c73532cacdbba16bf8e1ae4006e03cb

More bug fixes:
I fixed the bug that broke playThirty as noted in commit b7ed4474. Turns out it was a pass by reference error, oops

src/main/java/common/AI.java
@@ -29,7 +29,7 @@ public abstract class AI extends Entity implements AIType {
 					message += (meldList.get(i).getTileAt(j).getColour() + " " + meldList.get(i).getTileAt(j).getRank() + ", ");
 				
 				tm.addMeldToBoardMeld(meldList.get(i));
-				this.hand.tiles = removeFromMeld(this.hand.tiles, meldList.get(i));
+				this.removeFromHand(meldList.get(i));
 					
 			}
 				
@@ -192,7 +192,7 @@ public abstract class AI extends Entity implements AIType {
 					message += (meldList.get(i).getTileAt(j).getColour() + " " + meldList.get(i).getTileAt(j).getRank() + ", ");
 			
 				tm.addMeldToBoardMeld(meldList.get(i));
-				this.hand.tiles = removeFromMeld(this.hand.tiles, meldList.get(i));
+				this.removeFromHand(meldList.get(i));
 			}
 			System.out.println(message.substring(0, message.length() - 2));
 		}
@@ -287,29 +287,32 @@ public abstract class AI extends Entity implements AIType {
 			return false;
 		}
 	}
-/* TODO: for later usage
+
 	// Removes a Meld (i.e. a set of Tile objects) from the hand of an AI.
 	public void removeFromHand(Meld meld) {
 
-		if (this.hand.tiles == null || meld.tiles == null)
+		Meld tempMeld = new Meld();
+		tempMeld.tiles.addAll(meld.tiles);
+		
+		if (this.hand.tiles == null || tempMeld.tiles == null)
 			return;
 
-		if (this.hand.tiles.isEmpty() || meld.tiles.isEmpty() || this.hand.tiles.size() < meld.tiles.size())
+		if (this.hand.tiles.isEmpty() || tempMeld.tiles.isEmpty() || this.hand.tiles.size() < tempMeld.tiles.size())
 			return;
 
-		while (!meld.tiles.isEmpty()) {
+		while (!tempMeld.tiles.isEmpty()) {
 
 			for (int i = 0; i < this.hand.tiles.size(); i++) {
-				if (this.hand.tiles.get(i).getColour().equals(meld.tiles.get(0).getColour())
-				 && this.hand.tiles.get(i).getRank() == meld.tiles.get(0).getRank()) {
+				if (this.hand.tiles.get(i).getColour().equals(tempMeld.tiles.get(0).getColour())
+				 && this.hand.tiles.get(i).getRank() == tempMeld.tiles.get(0).getRank()) {
 					this.hand.tiles.remove(i);
 					break;
 				}
 			}
-			meld.tiles.remove(0);
+			tempMeld.tiles.remove(0);
 		}
 	}
-*/	
+	
 	public List<Tile> removeFromMeld(List<Tile> hand, Meld meld) {
 
 		List<Tile> tempHand = new ArrayList<Tile>();